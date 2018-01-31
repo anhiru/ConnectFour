ee4c8e646881c0b6302558f1d9eee794e3488a0b
Andrew Tran <Andrew@andrews-mbp.esuhsd.org>
2018-01-31 10:04:15 -0800

    fixed Player# color on yellow start

--- Board.java
+++ Board.java
@@ -180,7 +180,7 @@ class Board extends JComponent {
 		} 
 		if(colorStart == 'y') {
 			redTurn = true;
-			firstPiece = false;
+			firstPiece = true;
 		}
 		for(int i = 0; i < row; i++) {
 			for(int j = 0; j < col; j++) {
